3eb667be334b221b1cd340cb5093fcf8bfbec186
ladheniliya <58031925+ladheniliya@users.noreply.github.com>
2021-02-25 18:33:02 +0530

    Create bash.sh

---
+++ assignment1/bash.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# chmod +x quesNo1.py
+
+python3 q1.py >> tmp/output/1.csv
+echo "Ques No 1 complete"
+
+python3 q2.py >> tmp/output/2.csv
+echo "Ques No 2 complete"
+
+python3 q3.py >> tmp/output/3.csv
+echo "Ques No 3 complete"
+
+python3 q4.py >> tmp/output/4.csv
+echo "Ques No 4 complete"
+
+python3 q5.py >> tmp/output/5.csv
+echo "Ques No 5 complete"
