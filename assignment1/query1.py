d6525dc95fe2de602a5a393825dc9243ccf1171d
ladheniliya <58031925+ladheniliya@users.noreply.github.com>
2021-02-25 18:38:16 +0530

    Update query1.py

--- assignment1/query1.py
+++ assignment1/query1.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 #1. How many LEED projects are there in Virginia (including all types of project types and versions of LEED)?
 from merge import virginiaData
 Virginia_data = virginiaData()
