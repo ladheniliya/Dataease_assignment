0749da98e1500ae7190eae7479792a7f4dd1fecd
ladheniliya <58031925+ladheniliya@users.noreply.github.com>
2021-02-25 18:37:26 +0530

    Create query1.py

---
+++ assignment1/query1.py
@@ -0,0 +1,10 @@
+
+#1. How many LEED projects are there in Virginia (including all types of project types and versions of LEED)?
+from merge import virginiaData
+Virginia_data = virginiaData()
+
+q1 = Virginia_data.select("ID","ProjectName","LEEDSystemVersionDisplayName","ProjectTypes","State")
+q1.show(truncate=False)
+
+print("\nSchema")
+print(q1.schema)
